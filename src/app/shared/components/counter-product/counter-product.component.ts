4c71622559135dcd4abab05379707759ffa301aa
Allison <allis2897@gmail.com>
2025-03-20 21:23:24 -0600

    Se crean componentes de card, rating,counter

---
+++ src/app/shared/components/counter-product/counter-product.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-product',
+  templateUrl: './counter-product.component.html',
+  styleUrls: ['./counter-product.component.css']
+})
+export class CounterProductComponent {
+
+}
--- src/app/shared/shared-module.module.ts
+++ src/app/shared/shared-module.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { BtnCircleComponent } from './components/btn-circle/btn-circle.component';
 import { BtnDescuentosComponent } from './components/btn-descuentos/btn-descuentos.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CardProductComponent } from './components/card-product/card-product.component';
+import { RaitingComponent } from './components/raiting/raiting.component';
+import { CounterProductComponent } from './components/counter-product/counter-product.component';
+import { CategoryComponent } from './components/category/category.component';
 
 
 
@@ -10,7 +14,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   declarations: [
     BtnDescuentosComponent,
     BtnCircleComponent,
-    NavbarComponent
+    NavbarComponent,
+    CardProductComponent,
+    RaitingComponent,
+    CounterProductComponent,
+    CategoryComponent
   ],
   imports: [
     CommonModule
@@ -18,7 +26,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   exports:[
     BtnDescuentosComponent,
     BtnCircleComponent,
-    NavbarComponent
+    NavbarComponent,
+    CardProductComponent,
+    RaitingComponent,
+    CounterProductComponent,
+    CategoryComponent
   ]
 })
 export class SharedModuleModule { }
