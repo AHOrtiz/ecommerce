637b316395e3760c40b5cfc37a13077523a14ad3
Allison <allis2897@gmail.com>
2025-04-17 20:28:52 -0600

    Se crea dseño de login y register

--- src/app/shared/components/categories/categories.component.ts
+++ src/app/shared/components/categories/categories.component.ts
@@ -9,12 +9,20 @@ export class CategoriesComponent {
   @Input() title: string = '';
   @Input() categories:Array<Categories> = new Array();
   @ViewChild('container',{static:true}) containerElement?: ElementRef<HTMLDivElement>;
+  @ViewChild('containerCategories',{static:true}) containerCategories?: ElementRef<HTMLDivElement>;
+  @ViewChild('icon',{static:true}) icon?: ElementRef<HTMLLabelElement>;
   public isOpen: Boolean = false
   public toggle(){
     this.isOpen = !this.isOpen;
 
     if(this.isOpen){
 
+       this.containerCategories!.nativeElement.style.display = 'block';
+       this.icon!.nativeElement.classList.add('rotate');
+    }else{
+       this.containerElement!.nativeElement.style.height = '58px';
+       this.containerCategories!.nativeElement.style.display = 'none';
+       this.icon!.nativeElement.classList.remove('rotate');
     }
   }
 
