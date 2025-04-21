36fd3b3ea9fb9fe7b94b8e97631284f9cba6fdbc
Irwing-Herrera <irwing-herrera-2b@hotmail.com>
2025-04-20 21:14:35 -0600

    Actualizar Categorias

--- src/app/shared/components/categories/categories.component.ts
+++ src/app/shared/components/categories/categories.component.ts
@@ -7,42 +7,43 @@ import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@
 })
 export class CategoriesComponent {
   @Input() title: string = '';
-  @Input() categories:Array<Categories> = new Array();
+  @Input() categories: Array<Categories> = new Array();
   @Output() categorySelected = new EventEmitter<Categories>();
 
-  @ViewChild('container',{static:true}) containerElement?: ElementRef<HTMLDivElement>;
-  @ViewChild('containerCategories',{static:true}) containerCategories?: ElementRef<HTMLDivElement>;
-  @ViewChild('icon',{static:true}) icon?: ElementRef<HTMLLabelElement>;
+  @ViewChild('container', { static: true }) headerContainer?: ElementRef<HTMLDivElement>;
+  @ViewChild('bodyContainer', { static: true }) bodyContainer?: ElementRef<HTMLDivElement>;
+  @ViewChild('icon', { static: true }) icon?: ElementRef<HTMLLabelElement>;
+  
   public selectedCategory: Categories | null = null;
-
-selectCategory(category: Categories) {
-  this.selectedCategory = category;
-  this.categorySelected.emit(category);
-}
-
   public isOpen: Boolean = false;
-  public toggle(){
-    this.isOpen = !this.isOpen;
 
-    if(this.isOpen){
-
-       this.containerCategories!.nativeElement.style.display = 'block';
-       this.icon!.nativeElement.classList.add('rotate');
-    }else{
-       this.containerElement!.nativeElement.style.height = '58px';
-       this.containerCategories!.nativeElement.style.display = 'none';
-       this.icon!.nativeElement.classList.remove('rotate');
-    }
+  public selectCategory(category: Categories) {
+    this.selectedCategory = category;
+    this.categorySelected.emit(category);
   }
 
+  public toggle() {
+    this.isOpen = !this.isOpen;
 
+    if (this.isOpen) {
+      // BODY
+      this.bodyContainer!.nativeElement.style.display = 'flex';
+      // ICON
+      this.icon!.nativeElement.classList.add('rotate');
+    } else {
+      // BODY
+      this.bodyContainer!.nativeElement.style.display = 'none';
+      // ICON
+      this.icon!.nativeElement.classList.remove('rotate');
+    }
+  }
 }
 
- export enum Categories{
-     bedroom = "recamara",
-     livingRoom = "sala",
-     office = "oficina",
-     kitchen = "cocina",
-     outdoor = "exterior",
-     decor = "decoracion"
+export enum Categories {
+  bedroom = "recamara",
+  livingRoom = "sala",
+  office = "oficina",
+  kitchen = "cocina",
+  outdoor = "exterior",
+  decor = "decoracion"
 }
